c85bae474eef2151bb04b7970de7d4ccb9951b23
franck cuny <franck@lumberjaph.net>
2009-04-27 21:55:50 +0200

    doing the aggregation process

--- lib/MyAggregator.pm
+++ lib/MyAggregator.pm
@@ -1,32 +1,73 @@
 package MyAggregator;
-
+use feature ':5.10';
+use MyModel;
 use Moose;
-our $VERSION = '0.01';
+use MyAggregator::Entry;
+use KiokuDB;
+with 'MyAggregator::Roles::UserAgent', 'MyAggregator::Roles::Feed';
+
+has 'context' => ( is => 'ro', isa => 'HashRef' );
+has 'schema' => (
+    is      => 'ro',
+    isa     => 'Object',
+    lazy    => 1,
+    default => sub { MyModel->connect( $_[0]->context->{ dsn } ) },
+);
+has 'kioku' => (
+    is      => 'rw',
+    isa     => 'Object',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        KiokuDB->connect( $self->context->{ kioku_dir }, create => 1 );
+    }
+);
+
+sub run {
+    my $self = shift;
+
+    my $feeds = $self->schema->resultset( 'Feed' )->search();
+    while ( my $feed = $feeds->next ) {
+        my $res = $self->fetch_feed( $feed->url );
+        if ( !$res || !$res->is_success ) {
+            say "can't fetch " . $feed->url;
+        } else {
+            $self->dedupe_feed( $res, $feed->id );
+        }
+    }
+}
+
+sub dedupe_feed {
+    my ( $self, $res, $feed_id ) = @_;
+
+    my $feed = $self->feed_parser( \$res->content );
+    return if ( !$feed );
+    foreach my $entry ( $feed->entries ) {
+        next if $self->schema->resultset( 'Entry' )->find( $entry->link );
+        my $meme = MyAggregator::Entry->new(
+            permalink => $entry->link,
+            title     => $entry->title,
+            author    => $entry->author,
+            date      => $entry->issued,
+            content   => $entry->content->body,
+        );
+        $self->kioku->txn_do(
+            scope => 1,
+            body  => sub {
+                $self->kioku->insert( $meme->id => $meme );
+            }
+        );
+        $self->schema->txn_do(
+            sub {
+                $self->schema->resultset( 'Entry' )->create(
+                    {   entryid   => $meme->id,
+                        permalink => $meme->permalink,
+                        feedid    => $feed_id,
+                    }
+                );
+            }
+        );
+    }
+}
 
 1;
-__END__
-
-=head1 NAME
-
-MyAggregator -
-
-=head1 SYNOPSIS
-
-  use MyAggregator;
-
-=head1 DESCRIPTION
-
-MyAggregator is
-
-=head1 AUTHOR
-
-franck cuny E<lt>franck.cuny {at} rtgi.frE<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
